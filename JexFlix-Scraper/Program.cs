f261053d55d333552c3af7c94a198ece61d7d03c
Justin <justin@garofolo.net>
2019-02-02 05:59:08 -0500

    initial commit

--- JexFlix-Scraper/Program.cs
+++ JexFlix-Scraper/Program.cs
@@ -4,9 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace JexFlix_Scraper {
+namespace JexFlix {
+
     class Program {
+
         static void Main(string[] args) {
+
         }
+
     }
+
 }
