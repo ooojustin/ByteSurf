0c6bc5a8ca2e845affdc26a8a1182f39f09a1067
nexzoz <nexzozgaming@gmail.com>
2019-02-02 04:25:16 -0800

    Scrapes movie names
    Pretty much setup aside from getting dl links.

---
+++ JexFlix-Scraper/CookieAwareWebClient.cs
@@ -0,0 +1,23 @@
+﻿using CloudFlareUtilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JexFlix_Scraper {
+    public class CookieAwareWebClient : WebClient {
+
+        private CookieContainer Cookies = ClearanceHandler._cookies;
+
+        protected override WebRequest GetWebRequest(Uri address) {
+            WebRequest request = base.GetWebRequest(address);
+            if (request is HttpWebRequest) {
+                HttpWebRequest httpRequest = request as HttpWebRequest;
+                httpRequest.CookieContainer = Cookies;
+            }
+            return request;
+        }
+    }
+}
