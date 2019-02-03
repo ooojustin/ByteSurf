b2d17691ffa4e94d88f9b1d7cd8640f230bbef75
nexzoz <nexzozgaming@gmail.com>
2019-02-03 01:35:01 -0800

    null check

--- JexFlix-Scraper/flixify.cs
+++ JexFlix-Scraper/flixify.cs
@@ -8,7 +8,6 @@ namespace JexFlix_Scraper {
 
     public class ServerData {
         public List<Item> items { get; set; }
-        public List<Filter> filters { get; set; }
         public int total { get; set; }
         public int page { get; set; }
         public int items_per_page { get; set; }
@@ -34,8 +33,8 @@ namespace JexFlix_Scraper {
         public List<string> genres { get; set; }
         public double? rating { get; set; }
         public string imdb_id { get; set; }
-        public int year { get; set; }
-        public DateTime released { get; set; }
+        public int? year { get; set; }
+        public DateTime? released { get; set; }
         public long released_sec_ago { get; set; }
         public Images images { get; set; }
     }
@@ -47,16 +46,5 @@ namespace JexFlix_Scraper {
         public string active { get; set; }
         public string type { get; set; }
         public string value { get; set; }
-    }
-
-    public class Filter {
-        public string url { get; set; }
-        public string @class { get; set; }
-        public string title { get; set; }
-        public string param { get; set; }
-        public string value { get; set; }
-        public string type { get; set; }
-        public string active { get; set; }
-        public List<Item2> items { get; set; }
     }
 }
