44597c4969324c742610e6bb36bac99767fc2031
Justin <justin@garofolo.net>
2019-02-14 00:28:02 -0500

    login & create_account funcs

--- www.jexflix.com/inc/utils.php
+++ www.jexflix.com/inc/utils.php
@@ -4,11 +4,20 @@
 	// Functions used generally in other parts of the website.
 
 	function login($email, $password) {
-
+		global $db;
+		$check_login = $db->prepare('SELECT * FROM users WHERE email=:email');
+		$check_login->bindValue(':email', $email);
+		$check_login->execute();
+		$user = $check_login->fetch();
+		return $user && password_verify($password, $user['password']);
 	}
 
 	function create_account($email, $password) {
-
+		global $db;
+		$create_account = $db->prepare('INSERT INTO users (email, password) VALUES (:email, :password)');
+		$create_account->bindValue(':email', $email);
+		$create_account->bindValue(':password', password_hash($password, PASSWORD_BCRYPT));
+		$create_account->execute();
 	}
 
 ?>
