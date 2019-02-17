8b1347dbaa85a05c3710497af7d98ead5dfca2f5
nexzoz <nexzozgaming@gmail.com>
2019-02-17 07:17:43 -0800

    catalog sessions, logo, logout, footer, header

--- www.jexflix.com/catalog/index.php
+++ www.jexflix.com/catalog/index.php
@@ -1,7 +1,21 @@
 <?php
+    // catalog page
+    session_start();
+    
+    if (!isset($_SESSION['id'])) {
+        header("location: /login");
+        die();
+    }
+    
+   if (isset($_GET['logout'])) {
+  	    session_destroy();
+  	    unset($_SESSION['id']);
+        header("location: ../login");
+   	    die();
+   }
+
 
 	require '../inc/server.php';
-    require 'utils.php';
 
     // all variables and their default values
     $vars = array(
@@ -36,21 +50,21 @@
      	if (in_array($var, $vars_containify)) {
      		//$get_movies->bindValue($binder, '"%' . strtolower($$var) . '%"');
      		$querystr = str_replace($binder, '"%' . strtolower($$var) . '%"', $querystr);
-     		echo $binder . ' = %' . $$var . '%'; 
+     		//echo $binder . ' = %' . $$var . '%'; 
      	} else {
      		//$get_movies->bindValue($binder, $$var);
      		$querystr = str_replace($binder, $$var, $querystr);
-     		echo $binder . ' = ' . $$var;
+     		//echo $binder . ' = ' . $$var;
      	}
-     	echo PHP_EOL;
+     	//echo PHP_EOL;
 
     }
 
-    echo PHP_EOL . $querystr . PHP_EOL;
+    //echo PHP_EOL . $querystr . PHP_EOL;
     $get_movies = $db->prepare($querystr);
     $get_movies->execute();
 
-    echo $get_movies->rowCount() . PHP_EOL;
+   //echo $get_movies->rowCount() . PHP_EOL;
 
 ?>
 <!DOCTYPE html>
@@ -95,7 +109,7 @@
 						<div class="header__content">
 							<!-- header logo -->
 							<a href="../home" class="header__logo">
-								<img src="../img/logo.svg" alt="">
+								<img src="../img/logo.png" alt="">
 							</a>
 							<!-- end header logo -->
 
@@ -128,9 +142,6 @@
 									<ul class="dropdown-menu header__dropdown-menu" aria-labelledby="dropdownMenuMore">
 										<li><a href="../about">About</a></li>
 										<li><a href="../profile">Profile</a></li>
-										<li><a href="../signin">Sign In</a></li>
-										<li><a href="../signup">Sign Up</a></li>
-										<li><a href="../404">404 Page</a></li>
 									</ul>
 								</li>
 								<!-- end dropdown -->
@@ -150,14 +161,13 @@
 									<ul class="dropdown-menu header__dropdown-menu" aria-labelledby="dropdownMenuLang">
 										<li><a href="#">English</a></li>
 										<li><a href="#">Spanish</a></li>
-										<li><a href="#">Russian</a></li>
 									</ul>
 								</div>
 								<!-- end dropdown -->
 
-								<a href="signin.html" class="header__sign-in">
+								<a href="index.php?logout=1" class="header__sign-in">
 									<i class="icon ion-ios-log-in"></i>
-									<span>sign in</span>
+									<span>Sign Out</span>
 								</a>
 							</div>
 							<!-- end header auth -->
@@ -194,19 +204,19 @@
 	<!-- end header -->
 
 	<!-- page title -->
-	<section class="section section--first section--bg" data-bg="../img/section/section.jpg">
+	<section class="section section--first section--bg">
 		<div class="container">
 			<div class="row">
 				<div class="col-12">
 					<div class="section__wrap">
 						<!-- section title -->
-						<h2 class="section__title">Catalog grid</h2>
+						<h2 class="section__title">Movie Catalog</h2>
 						<!-- end section title -->
 
 						<!-- breadcrumb -->
 						<ul class="breadcrumb">
-							<li class="breadcrumb__item"><a href="#">Home</a></li>
-							<li class="breadcrumb__item breadcrumb__item--active">Catalog grid</li>
+							<li class="breadcrumb__item"><a href="../home">Home</a></li>
+							<li class="breadcrumb__item breadcrumb__item--active">Movie Catalog</li>
 						</ul>
 						<!-- end breadcrumb -->
 					</div>
@@ -383,22 +393,12 @@
 		<div class="container">
 			<div class="row">
 				<!-- footer list -->
-				<div class="col-12 col-md-3">
-					<h6 class="footer__title">Download Our App</h6>
-					<ul class="footer__app">
-						<li><a href="#"><img src="../img/Download_on_the_App_Store_Badge.svg" alt=""></a></li>
-						<li><a href="#"><img src="../img/google-play-badge.png" alt=""></a></li>
-					</ul>
-				</div>
-				<!-- end footer list -->
-
-				<!-- footer list -->
 				<div class="col-6 col-sm-4 col-md-3">
 					<h6 class="footer__title">Resources</h6>
 					<ul class="footer__list">
-						<li><a href="#">About Us</a></li>
-						<li><a href="#">Pricing Plan</a></li>
-						<li><a href="#">Help</a></li>
+						<li><a href="../about">About Us</a></li>
+						<li><a href="../pricing">Pricing Plan</a></li>
+						<li><a href="../faq">Help</a></li>
 					</ul>
 				</div>
 				<!-- end footer list -->
@@ -407,9 +407,8 @@
 				<div class="col-6 col-sm-4 col-md-3">
 					<h6 class="footer__title">Legal</h6>
 					<ul class="footer__list">
-						<li><a href="#">Terms of Use</a></li>
-						<li><a href="#">Privacy Policy</a></li>
-						<li><a href="#">Security</a></li>
+						<li><a href="../tos">Terms of Use</a></li>
+						<li><a href="../privacy">Privacy Policy</a></li>
 					</ul>
 				</div>
 				<!-- end footer list -->
@@ -418,14 +417,8 @@
 				<div class="col-12 col-sm-4 col-md-3">
 					<h6 class="footer__title">Contact</h6>
 					<ul class="footer__list">
-						<li><a href="tel:+18002345678">+1 (800) 234-5678</a></li>
-						<li><a href="mailto:support@moviego.com">support@flixgo.com</a></li>
-					</ul>
-					<ul class="footer__social">
-						<li class="facebook"><a href="#"><i class="icon ion-logo-facebook"></i></a></li>
-						<li class="instagram"><a href="#"><i class="icon ion-logo-instagram"></i></a></li>
-						<li class="twitter"><a href="#"><i class="icon ion-logo-twitter"></i></a></li>
-						<li class="vk"><a href="#"><i class="icon ion-logo-vk"></i></a></li>
+					    <li><a href="../discord">Discord</a></li>
+						<li><a href="mailto:support@jexflix.com">support@jexflix.com</a></li>
 					</ul>
 				</div>
 				<!-- end footer list -->
@@ -433,11 +426,11 @@
 				<!-- footer copyright -->
 				<div class="col-12">
 					<div class="footer__copyright">
-						<small>© 2018 FlixGo. Create by <a href="https://themeforest.net/user/dmitryvolkov/portfolio?ref=DmitryVolkov" target="_blank">Dmitry Volkov</a></small>
+						<small class="section__text">© 2019 jexflix. Created by <a href="https://i.imgur.com/gEZ5bko.jpg" target="_blank">Anthony Almond</a></small>
 
 						<ul>
-							<li><a href="#">Terms of Use</a></li>
-							<li><a href="#">Privacy Policy</a></li>
+							<li><a href="../tos">Terms of Use</a></li>
+							<li><a href="../privacy">Privacy Policy</a></li>
 						</ul>
 					</div>
 				</div>
