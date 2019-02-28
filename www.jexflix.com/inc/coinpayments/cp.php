1381116d20c880c0762f9297fe05797e8065b7d8
Justin <justin@garofolo.net>
2019-02-28 04:53:59 -0500

    more coinpayments backend code

--- www.jexflix.com/inc/coinpayments/cp.php
+++ www.jexflix.com/inc/coinpayments/cp.php
@@ -1,19 +1,20 @@
 <?php
 
 	require dirname(__FILE__) . '/CoinpaymentsAPI.php';
+	require dirname(__FILE__) . '/utils.php';
 
 	// coinpayments api keys
 	define('PRIVATE_KEY', 'dB05204b1023751C8EC41eBB04b0efE255359b849fA9578e21259Bc66eDe7668');
 	define('PUBLIC_KEY', '34d1d824c6fcd47436863b34beaa7487d29a39f40af249eb115719c97607d6f2');
 
 	// btc address to receive payments
-	define('BTC_ADDRESS', '1AnSFnaAgvtrsXZm82ZCqYv7M5c8cLKv64');
+	define('BTC_ADDRESS', '3GiE1z7zQyvYRcxybcAx4jmsomqqVz3ngu');
 
 	// ipn url (to handle payment status updates)
 	define('IPN_URL', 'https://jexflix.com/inc/coinpayments/ipn.php');
 
 	// initialize CoinpaymentsAPI object
-	$GLOBALS['cp'] = new CoinpaymentsAPI(PRIVATE_KEY, PUBLIC_KEY);
+	$GLOBALS['cp'] = new CoinpaymentsAPI(PRIVATE_KEY, PUBLIC_KEY, 'json');
 
 	function create_btc_payment($amount, $email, $name, $product_name, $product_number) {
 
@@ -32,6 +33,9 @@
 			'custom_info', // anything (custom information to store)
 			IPN_URL // URL to IPN to update/handle payments
 		);
+
+		// create_order($user, $amount, $method)
+		create_order($name, $amount, 'btc');
 
 		return $data;
 
