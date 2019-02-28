8cad5e6571da75466d551156d61d3450e3d4dc2d
Justin <justin@garofolo.net>
2019-02-28 05:56:18 -0500

    finished coinpayments setup

--- www.jexflix.com/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	require 'inc/coinpayments/cp.php';
-
-	$data = create_btc_payment(5, 'justin@garofolo.net', 'Justin Garofolo', 'Test Product', '1');
-	var_dump($data);
-	//die(generate_invoice_string());
-
-?>
