6a3db185dd0bbf4c82bc78ff6b2799945e569441
Justin <justin@garofolo.net>
2019-02-28 19:00:34 -0500

    some neat code i wrote (test it, somebody)

---
+++ www.jexflix.com/inc/session.php
@@ -0,0 +1,35 @@
+<?php
+	
+	session_start();
+
+	// establish user data
+	if (isset($_SESSION['id']))
+		$GLOBALS['user'] = get_user_by_id($_SESSION['id']);
+
+	// handle logout
+	if (isset($_GET['logout'])) {
+        session_destroy();
+        unset($_SESSION['id']);
+        header("location: ../login");
+        die();
+    }
+
+    // function to require a login
+	function require_login() {
+		if (!isset($_SESSION['id'])) {
+			header("location: https://jexflix.com/login/");
+        	die();
+       	}
+	}
+
+	// function to require a subscription
+	function require_subscription() {
+		require_login(); // must be logged in to check subscription
+		global $user;
+		if (time() <= intval($user['expires'])) {
+			header("location: https://jexflix.com/pricing/");
+			die();
+		}
+	}
+
+?>
