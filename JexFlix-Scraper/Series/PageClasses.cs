01b22157d9753132ed12faf3cf83c3b8fe6b5083
nexzoz <nexzozgaming@gmail.com>
2019-03-02 22:46:29 -0800

    started on scraper for tv shows

---
+++ JexFlix-Scraper/Series/PageClasses.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JexFlix_Scraper.Series {
+    class PageClasses {
+        public class Images {
+            public string preview { get; set; }
+            public string poster { get; set; }
+            public string preview_list { get; set; }
+            public string preview_grid { get; set; }
+        }
+
+        public class Item {
+            public string id { get; set; }
+            public string type { get; set; }
+            public string url { get; set; }
+            public string title { get; set; }
+            public string description { get; set; }
+            public string slug { get; set; }
+            public string certification { get; set; }
+            public int? duration { get; set; }
+            public List<object> genres { get; set; }
+            public double? rating { get; set; }
+            public string imdb_id { get; set; }
+            public int year { get; set; }
+            public DateTime released { get; set; }
+            public int released_sec_ago { get; set; }
+            public int last_seq { get; set; }
+            public Images images { get; set; }
+        }
+
+        public class Item2 {
+            public string url { get; set; }
+            public object @class { get; set; }
+            public string title { get; set; }
+            public string active { get; set; }
+            public string type { get; set; }
+            public string value { get; set; }
+        }
+
+        public class Filter {
+            public string url { get; set; }
+            public string @class { get; set; }
+            public string title { get; set; }
+            public string param { get; set; }
+            public string value { get; set; }
+            public string type { get; set; }
+            public string active { get; set; }
+            public List<Item2> items { get; set; }
+        }
+
+        public class RootObject {
+            public List<Item> items { get; set; }
+            public List<Filter> filters { get; set; }
+            public int total { get; set; }
+            public int page { get; set; }
+            public int items_per_page { get; set; }
+            public string ts { get; set; }
+        }
+
+    }
+}
