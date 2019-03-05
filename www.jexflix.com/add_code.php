d1e386eaa18c8ab19f99cb9509dbe455b62c6a7d
Justin <justin@garofolo.net>
2019-03-05 01:00:26 -0500

    improved add_code.php

--- www.jexflix.com/add_code.php
+++ www.jexflix.com/add_code.php
@@ -1,12 +1,20 @@
 <?php
 
     require 'inc/server.php';
+    define('SECONDS_PER_DAY', 86400);
     
-    if (!isset($_GET['user'])) die();
-    if (!isset($_GET['amount'])) die();
+    if (!isset($_GET['user'])) 
+    	die('Please provide \'user\' variable in URL.');
+
+    $amount = 1; // number of keys
+    if (isset($_GET['amount']))
+    	$amount = intval($_GET['amount']);
+
+    $duration = 7; // in days
+    if (isset($_GET['duration']))
+    	$duration = intval($_GET['duration']);
     
-    for ($x = 0; $x <= $_GET['amount']; $x++) {
-        generate_trial_key($_GET['user'], 604800);
-    }
+    for ($i = 0; $i <= $amount; $i++)
+        generate_trial_key($_GET['user'], SECONDS_PER_DAY * $duration);
 
 ?>
