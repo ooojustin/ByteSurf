593986fda3b1e8dd74e4b4995b504cbaf092cc1b
Justin <justin@garofolo.net>
2019-03-16 22:54:09 -0400

    get_imdb_similar_titles func (not completely done yet)

--- www.jexflix.com/inc/imdb.php
+++ www.jexflix.com/inc/imdb.php
@@ -1,14 +1,36 @@
 <?php
 
+	function get_imdb_similar_titles($url, $convert_to_movie_urls = false) {
+		$imdb_data = get_imdb_page_data($url);
+		$regex = '/<a href="\/title\/.*?\/\?ref_=tt_rec_tti"\s><img height="113" width="76" alt=".*?" title=".*?" src=".*?" class="loadlate hidden rec_poster_img" loadlate=".*?" \/>/m';
+		if (preg_match_all($regex, $imdb_data, $matches) === FALSE)
+			return array();
+		$titles = array();
+		$matches = current($matches);
+		foreach ($matches as $match) {
+			$href = explode('"', $match)[1];
+			$title = explode('/', $href)[2];
+			array_push($titles, $title);
+		}
+		if ($convert_to_movie_urls) {
+			// todo
+		} else return $titles;
+	}
+
 	function get_imdb_rating($url) {
-		$movie_data = get_movie_data($url);
-		$imdb_url = 'https://www.imdb.com/title/' . $movie_data['imdb_id'];
-		$imdb_raw = file_get_contents($imdb_url);
+		$imdb_data = get_imdb_page_data($url);
 		$regex = '/<span class="rating">.*<span class="ofTen">\/10<\/span><\/span>/m';
-		if (!preg_match($regex, $imdb_raw, $matches))
+		if (!preg_match($regex, $imdb_data, $matches))
 			return doubleval(-1);
 		// forgive me lord for i have sinned:
 		return doubleval(explode('<', explode('>', $matches[0])[1])[0]);
+	}
+
+	function get_imdb_page_data($url) {
+		$movie_data = get_movie_data($url);
+		$imdb_url = 'https://www.imdb.com/title/' . $movie_data['imdb_id'];
+		$imdb_raw = file_get_contents($imdb_url);
+		return $imdb_raw;
 	}
 
 	function update_imdb_rating($url) {
