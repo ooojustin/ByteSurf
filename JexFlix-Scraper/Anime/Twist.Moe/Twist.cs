2e763d76872e4485c0e8490c7603c4d75194fe9d
Kite <weebhook@gmail.com>
2019-03-19 10:53:04 +1100

    Basic functions to fetch data. will add more.

--- JexFlix-Scraper/Anime/Twist.Moe/Twist.cs
+++ JexFlix-Scraper/Anime/Twist.Moe/Twist.cs
@@ -11,16 +11,8 @@ namespace JexFlix_Scraper.Anime.Twist.Moe {
 
         public static void Run() {
 
+            // Setup.
             Aligolia_Keys AligoliaKeys = KitsuAPI.GetAligoliaKeys();
-
-            Media_Production Media = KitsuAPI.GetMediaProduction(AligoliaKeys, "naruto");
-
-            int id = KitsuAPI.GetFirstTVID(Media);
-
-            KitsuAnime.Anime AnimeInfo = KitsuAPI.GetKitsuAnime(id);
-
-            Console.WriteLine(AnimeInfo.data.attributes.titles.en_jp);
-            Console.WriteLine(AnimeInfo.data.attributes.synopsis);
 
         }
     }
