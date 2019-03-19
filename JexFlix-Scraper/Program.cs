ec1b55cf4fce1b1676bdab4b651597290dd55caf
nexzoz <nexzozgaming@gmail.com>
2019-03-18 21:15:52 -0700

    fixed some stuff, dont change pls kite

--- JexFlix-Scraper/Program.cs
+++ JexFlix-Scraper/Program.cs
@@ -3,9 +3,6 @@ using System.IO;
 using System.Threading;
 using JexFlix_Scraper;
 using JexFlix_Scraper.Anime;
-using JexFlix_Scraper.Anime.DarkAnime;
-using JexFlix_Scraper.Anime.Misc;
-using JexFlix_Scraper.Anime.Twist.Moe;
 using JexFlix_Scraper.Flixify;
 using JexFlix_Scraper.Shows;
 
@@ -14,24 +11,23 @@ class Program {
     [STAThread]
     static void Main(string[] args) {
 
-        MessageHandler.Start();
+        // MessageHandler.Start();
 
         // Anime.Run();
 
+
         //Flixify.Run();
-        //Thread t1 = new Thread(() => Flixify.Run(0));
-        //t1.Start();
-        //Thread t2 = new Thread(() => Flixify.Run(8));
-        //t2.Start();
+        Thread t1 = new Thread(() => Flixify.Run(6));
+        t1.Start();
+        Thread t2 = new Thread(() => Flixify.Run(17));
+        t2.Start();
         //Thread t3 = new Thread(() => Flixify.Run(7));
         //t3.Start();
         //Thread t4 = new Thread(() => Flixify.Run(3));
         //t4.Start();
-        // Shows.Run();
-        // DarkAnime.Run();
 
-        Twist.Run();
-
+        Thread showThread = new Thread(() => Shows.Run());
+        showThread.Start();
 
         Console.ReadKey();
 
