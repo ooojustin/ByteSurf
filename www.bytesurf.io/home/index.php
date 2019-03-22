77ee2dda1bb3802cae55abf63f59757f70346598
Justin <justin@garofolo.net>
2019-03-22 05:14:39 -0400

    added trending movies (not done)

--- www.bytesurf.io/home/index.php
+++ www.bytesurf.io/home/index.php
@@ -1,3 +1,21 @@
+<?php
+
+	include '../inc/server.php';
+	include '../inc/session.php';
+	// require_subscription();
+
+	$trending = array(
+        'triple-frontier-2019',
+        'mortal-engines-2018',
+        'overlord-2018',
+        'aquaman-2018',
+        'isnt-it-romantic-2019',
+        'spider-man-into-the-spider-verse-2018',
+        'ralph-breaks-the-internet-2018',
+        'crazy-rich-asians-2018'
+    );
+
+?>
 <!DOCTYPE html>
 <!--[if IE 7]>
 <html class="ie ie7 no-js" lang="en-US">
@@ -151,18 +169,30 @@
 	<div class="container">
 		<div class="row">
 	    	<div class="slider-single-item">
-	    		<!--
+	    		<?php 
+	    			foreach ($trending as $url) { 
+
+	    				$movie = get_movie_data($url);
+
+	    				$duration_data = explode(':', gmdate('H:i', $movie['duration']));
+	    				$duration = intval($duration_data[0]) . 'h ' . $duration_data[1] . 'm';
+
+	    				$genres = json_decode($movie['genres'], true);
+
+	    		?>
 	    		<div class="movie-item">
 	    			<div class="row">
 	    				<div class="col-md-8 col-sm-12 col-xs-12">
 	    					<div class="title-in">
 			    				<div class="cate">
-			    					<span class="blue"><a href="#">Sci-fi</a></span>
+			    					<? foreach ($genres as $genre) { ?>
+			    					<span style="background-color: <?= get_genre_color($genre) ?>"><a href="#"><?= ucfirst($genre); ?></a></span>
+			    					<? } ?>
+			    					<!--<span class="blue"><a href="#">Sci-fi</a></span>
 			    					<span class="yell"><a href="#">Action</a></span>
-			    					<span class="orange"><a href="#">advanture</a></span>
+			    					<span class="orange"><a href="#">advanture</a></span>-->
 			    				</div>
-			    				<h1><a href="#">guardians of the<br>
-								galaxy <span>2015</span></a></h1>
+			    				<h1><a href="#"><?= $movie['title'] ?> <span><?= $movie['year'] ?></span></a></h1>
 								<div class="social-btn">
 									<a href="#" class="parent-btn"><i class="ion-play"></i> Watch Trailer</a>
 									<a href="#" class="parent-btn"><i class="ion-heart"></i> Add to Favorite</a>
@@ -177,10 +207,10 @@
 									</div>		
 								</div>
 			    				<div class="mv-details">
-			    					<p><i class="ion-android-star"></i><span>7.4</span> /10</p>
+			    					<p><i class="ion-android-star"></i> <span><?= $movie['rating'] ?></span> /10</p>
 			    					<ul class="mv-infor">
-			    						<li>  Run Time: 2h21’ </li>
-			    						<li>  Rated: PG-13  </li>
+			    						<li>  Run Time: <?= $duration ?> </li>
+			    						<li>  Rated: <?= $movie['certification'] ?>  </li>
 			    						<li>  Release: 1 May 2015</li>
 			    					</ul>
 			    				</div>
@@ -192,13 +222,12 @@
 	    				</div>
 	    				<div class="col-md-4 col-sm-12 col-xs-12">
 		    				<div class="mv-img-2">
-			    				<a href="#"><img src="../images/uploads/poster1.jpg" alt=""></a>
+			    				<a href="#"><img src="<?= str_replace('cdn.jexflix.com', 'jexflix.b-cdn.net', authenticate_cdn_url($movie['thumbnail'])) ?>" alt=""></a>
 			    			</div>
 		    			</div>
 	    			</div>	
 	    		</div>
-	    		-->
-
+	    		<? } ?>
 	    	</div>
 	    </div>
 	</div>
