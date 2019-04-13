8165f22c3a548594923890a5a0282a608948aca0
ju$tin <justin@garofolo.net>
2019-04-12 23:28:02 -0400

    a bunch of progress tracking stuff (works on movies)

---
+++ www.bytesurf.io/inc/progress_tracker.php
@@ -0,0 +1,96 @@
+<?php
+    
+    require 'server.php';
+    require 'session.php';
+    require_subscription();
+
+    $username = $GLOBALS['user']['username'];
+    
+    if (!isset($_GET['action']))
+        die('Action not provided.');
+
+    switch ($_GET['action']) {
+        
+        case 'save':
+            
+            // require parameters for title, type, time, and completed
+            $params = array('t', 'type', 'time', 'completed');
+            require_get_params($params);
+            
+            // make sure type is valid
+            validate_type($_GET['type']);
+            
+            // set season and episode to -1 if they're not provided
+            default_get_param('s', -1);
+            default_get_param('e', -1);
+            
+            // save current information to database
+            $completed = $_GET['completed'] === 'true';
+            save_progress($username, $_GET['t'], $_GET['type'], $_GET['time'], $completed, $_GET['s'], $_GET['e']);
+            die('Saved progress successfully: ' . $_GET['time']);
+            
+        case 'get':
+            
+            // require parameters for title, and type
+            $params = array('t', 'type');
+            require_get_params($params);
+            
+            // make sure type is valid
+            validate_type($_GET['type']);
+            
+            // get progress row from database
+            $progress = get_progress($username, $_GET['t'], $_GET['type']);
+            
+            // return progress (season, episode, time, completed)
+            if ($progress)
+                die(sprintf('%s,%s,%s,%s', $progress['season'], $progress['episode'], $progress['time'], $progress['completed']));
+            else
+                die('0,0,0,0');
+        
+    }
+
+    function validate_type($type) {
+        $types = array('movie', 'show', 'anime');
+        if (!in_array($type, $types))
+            die('Invalid type provided: ' . $type);
+    }
+
+    function require_get_params($params) {
+        foreach ($params as $param)
+            if (!isset($_GET[$param]))
+                die('Missing provided parameter: ' . $param);
+    }
+
+    function default_get_param($param, $value) {
+        if (!isset($_GET[$param]))
+            $_GET[$param] = $value;
+    }
+    
+    function get_progress($username, $title, $type) {
+        global $db;
+        $get_progress = $db->prepare('SELECT * FROM progress_tracker WHERE username=:username AND title=:title AND type=:type ORDER BY id DESC LIMIT 1');
+        $get_progress->bindValue(':username', $username);
+        $get_progress->bindValue(':title', $title);
+        $get_progress->bindValue(':type', $type);
+        $get_progress->execute();
+        return $get_progress->fetch();
+    }
+
+    function save_progress($username, $title, $type, $time, $completed, $season = -1, $episode = -1) {
+        global $db;
+        if (get_progress($username, $title, $type))
+            $query = 'UPDATE progress_tracker SET season=:season, episode=:episode, time=:time, completed=:completed WHERE username=:username AND title=:title AND type=:type';
+        else
+            $query = 'INSERT INTO progress_tracker (username, type, title, season, episode, time, completed) VALUES (:username, :type, :title, :season, :episode, :time, :completed)';
+        $save_progress = $db->prepare($query);
+        $save_progress->bindValue(':username', $username);
+        $save_progress->bindValue(':title', $title);
+        $save_progress->bindValue(':type', $type);
+        $save_progress->bindValue(':time', $time);
+        $save_progress->bindValue(':completed', $completed);
+        $save_progress->bindValue(':season', $season);
+        $save_progress->bindValue(':episode', $episode);
+        $save_progress->execute();
+    }
+
+?>
---
+++ www.bytesurf.io/js/progress.tracker.js
@@ -0,0 +1,132 @@
+
+window.setInterval(update_progress, 10000);
+var last_progress_update = 0;
+var progress_season, progress_episode, progress_time, progress_completed;
+
+function update_progress() {
+    
+    // make sure player object exists
+    if (typeof player === 'undefined')
+        return;
+    
+    // make sure an update is actually needed
+    if (player.currentTime == last_progress_update)
+        return;
+    
+    // get season/episode/title into array
+    let params = get_important_params();
+    
+    // determine type, make sure it's valid
+    params['type'] = get_media_type();
+    if (!is_media_type_valid(params['type']))
+        return;
+    
+    // add player time (seconds, as float) to params
+    params['time'] = player.currentTime;
+    
+    // determine whether or not it's completed (last 5% of video)
+    let completion_time = player.duration - (player.duration * 0.05);
+    params['completed'] = (player.currentTime >= completion_time).toString();
+    
+    // build url with given parameters
+    let query = jQuery.param(params);
+    let url = 'https://bytesurf.io/inc/progress_tracker.php?action=save&' + query;
+    
+    // send web request (save progress) and store update time
+    get_request(url, function(r) { });
+    last_progress_update = player.currentTime;
+        
+}
+
+$(document).ready(function() {
+    
+    // get season/episode/title into array
+    let params = get_important_params();
+    
+    // make sure we have title
+    if (!('t' in params))
+        return;
+
+    // get type, make sure it's valid
+    params['type'] = get_media_type();
+    if (!is_media_type_valid(params['type']))
+        return;
+    
+    // generate url to get information from
+    let query = jQuery.param(params);
+    let url = 'https://bytesurf.io/inc/progress_tracker.php?action=get&' + query;
+    
+    // send request and parse response
+    get_request(url, function(response) { 
+        
+        let data = response.split(',');
+        if (data.length != 4)
+            return;
+        
+        progress_season = data[0];
+        progress_episode = data[1];
+        progress_time = data[2];
+        progress_completed = data[3];
+        
+        if (progress_time == 0)
+            return;
+        
+        let applyTime = window.setInterval(function() {
+            if (player.readyState == 4) {
+                console.log('ready');
+                player.currentTime = progress_time;
+                window.clearInterval(applyTime);
+            }
+        }, 500);
+        
+    });
+    
+});
+
+function value_exists(params, key) {
+    return (key in params) && params['key'] > -1;
+}
+
+function get_important_params() {
+    
+    // find season, episode, and title.
+    // put them into a key/value array that we can build a query from.
+    let find = ['s', 'e', 't'];
+    let params = { };
+         
+    // initialize URL and URLSearchParams objects so we can get params from URL
+    let url_obj = new URL(window.location.href);
+    let searchParams = new URLSearchParams(url_obj.search);
+    
+    // put items from url into our own array
+    find.forEach(function(f) {
+        if (searchParams.has(f)) {
+            params[f] = searchParams.get(f);
+        }
+    });
+    
+    return params;
+    
+}
+
+function is_media_type_valid(type) {
+    let types = ['movie', 'show', 'anime'];
+    return types.includes(type);
+}
+
+function get_media_type() {
+    let path = window.location.pathname;
+    let file_name = path.substring(path.lastIndexOf('/') + 1);
+    let type = file_name.replace('.php', '');
+    return type;
+}
+
+function get_request(url, callback) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", url, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
