f0cd6a2d3961615e97c00f06c34be6cd0148d3ca
Kite <jackywen4@gmail.com>
2019-04-16 13:36:36 +1000

    turns out there is a nice php page called updater

--- www.bytesurf.io/anime/watched.php
+++
@@ -1,9 +0,0 @@
-<?php  
-    require '../inc/server.php';
-    require '../inc/session.php';
-    require_login();
-
-	// This php page takes the anime slug and the user id and the given episode and stores it in the database if watched.
-
-	
-?>
