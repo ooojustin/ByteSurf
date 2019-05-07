64bcd1319a673c5efdd2ec020052b70408cb1d3e
ju$tin <justin@garofolo.net>
2019-05-07 07:20:24 -0400

    base58_decode, validate_btc_address util funcs

--- www.bytesurf.io/inc/utils.php
+++ www.bytesurf.io/inc/utils.php
@@ -977,4 +977,51 @@
         die(gzcompress($txt, 9));
     }
 
+    function validate_btc_address($address){
+        
+        $decoded = base58_decode($address);
+        
+        $d1 = hash("sha256", substr($decoded,0,21), true);
+        $d2 = hash("sha256", $d1, true);
+        
+        // verify digest
+        if(substr_compare($decoded, $d2, 21, 4))
+            return false;
+        
+        return true;
+        
+    }
+    
+    function base58_decode($input) {
+        
+        $alphabet = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
+        $out = array_fill(0, 25, 0);
+        
+        for($i=0;$i<strlen($input);$i++){
+            
+            if(($p=strpos($alphabet, $input[$i]))===false)
+                return false; // invalid character
+            
+            $c = $p;
+            
+            for ($j = 25; $j--; ) {
+                $c += (int)(58 * $out[$j]);
+                $out[$j] = (int)($c % 256);
+                $c /= 256;
+                $c = (int)$c;
+            }
+            
+            if($c != 0)
+                return false; // address is too long
+            
+        }
+        
+        $result = "";
+        foreach($out as $val)
+            $result .= chr($val);
+ 
+        return $result;
+        
+    }
+
 ?>
