db2a8539bad9c54c64aef4a857e7b6b1b867abfc
ju$tin <justin@garofolo.net>
2019-06-02 13:52:11 -0400

    database.get_movie function

--- uploader/uploader/database.py
+++ uploader/uploader/database.py
@@ -12,3 +12,16 @@ db.autocommit = True # automatically commit changes to db (ex: insert queries)
 cursor = db.cursor() # object used to execute commands
 cursor.execute("USE " + db_cfg['database']) # start using the specified database on the MySQL server
 print("done")
+
+def get_movie(url):
+    """
+    Gets a movie from the database, based on it's url.
+
+    Parameters:
+        url (string): Last part of the url, used to uniquely identify the movie. (Example: 'the-grinch-2018')
+
+    Returns:
+        tuple: The movie row from the database.
+    """
+    cursor.execute("SELECT * FROM movies WHERE url = '{}'".format(url))
+    return cursor.fetchone()
