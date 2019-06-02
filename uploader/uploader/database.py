5aa979e3b96a99c39c73fa80689f6b3f3cd2e601
ju$tin <justin@garofolo.net>
2019-06-02 16:04:23 -0400

    check if database.cfg exists in database module (sphinx compatibility)

--- uploader/uploader/database.py
+++ uploader/uploader/database.py
@@ -1,17 +1,18 @@
-import mysql.connector, json
+import mysql.connector, json, os
 
 # initialize the database connection
-print("Initiailizing database connection...", end = " ")
-db_cfg = json.loads(open('database.cfg', 'r').read())
-db = mysql.connector.connect(
-    host = db_cfg['host'],
-    user = db_cfg['username'],
-    password = db_cfg['password']
-)
-db.autocommit = True # automatically commit changes to db (ex: insert queries)
-cursor = db.cursor() # object used to execute commands
-cursor.execute("USE " + db_cfg['database']) # start using the specified database on the MySQL server
-print("done")
+if os.path.isfile('database.cfg'):
+    print("Initiailizing database connection...", end = " ")
+    db_cfg = json.loads(open('database.cfg', 'r').read())
+    db = mysql.connector.connect(
+        host = db_cfg['host'],
+        user = db_cfg['username'],
+        password = db_cfg['password']
+    )
+    db.autocommit = True # automatically commit changes to db (ex: insert queries)
+    cursor = db.cursor() # object used to execute commands
+    cursor.execute("USE " + db_cfg['database']) # start using the specified database on the MySQL server
+    print("done")
 
 def get_movie(url):
     """
