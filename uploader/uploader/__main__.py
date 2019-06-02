e726b56c870a3c49b512569e34b749cfa197b509
ju$tin <justin@garofolo.net>
2019-06-02 01:47:38 -0400

    fixed bug
    note: ends at page 100. need to add genre support or something to scrape all movies.

--- uploader/uploader/__main__.py
+++ uploader/uploader/__main__.py
@@ -2,16 +2,21 @@ import flixify
 
 flixify = flixify.login("justin@garofolo.net", "D3MU&DvWm9%xf*z")
 page = 1
+count = 1
 
 while True:
 
     data = flixify.download_data("movies", page)
+    if not data:
+        print("failed: download_data('{}', {})".format("movies", page))
+        break
 
     movies = data['items']
     if len(movies) == 0:
         break
 
     for movie in movies:
-        print(movie['title'] + " - " + movie['url'])
+        print(str(count) + ": " + movie['title'])
+        count += 1
 
     page += 1
