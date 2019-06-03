218d17fa83a7154f569747e2d6e6e2d5cf7c6b0c
ju$tin <justin@garofolo.net>
2019-06-03 14:15:57 -0400

    testing cdn uploader in main module

--- uploader/uploader/__main__.py
+++ uploader/uploader/__main__.py
@@ -1,25 +1,47 @@
-import flixify
+import flixify, cdn, database
 
 scraper = flixify.login("justin@garofolo.net", "D3MU&DvWm9%xf*z")
-page = 1
-count = 1
 
-while True:
+data = scraper.download_data("movies", 1)
+movies = data["items"]
 
-    genre = flixify.GENRES[0]
-    data = scraper.download_data("movies", page, genre)
-
-    # if we're out of videos
-    if not data:
-        print("failed: download_data('{}', {})".format("movies", page))
+for m in movies:
+    if m["url"].endswith("captain-marvel-2019"):
+        movie = m
         break
 
-    movies = data['items']
-    if len(movies) == 0:
-        break
+movie = scraper.get_movie_data(movie)
+cdn.upload_movie(movie)
+print("completed")
 
-    for movie in movies:
-        print(str(count) + ": " + movie['title'])
-        count += 1
-
-    page += 1
+# for genre in flixify.GENRES:
+#
+#     page = 1
+#
+#     while True:
+#
+#         data = scraper.download_data("movies", page, genre)
+#
+#         # if we're out of videos
+#         if not data:
+#             print("FINISHED GENRE: " + genre)
+#             break
+#
+#         movies = data['items']
+#         if len(movies) == 0:
+#             break
+#
+#         for movie in movies:
+#
+#             # fix movie url (remove the '/movies/' text from the start)
+#             url = movie["url"].replace("/movies/", "")
+#
+#             # skip this movie if it already exists on the server
+#             if database.get_movie(movie["url"]) is not None: continue
+#
+#             # print movie title
+#             print("Movie found: " + movie["title"] + " (genre: {})".format(genre))
+#
+#         page += 1
+#
+# print("COMPLETED")
