5b007e9be2e2cbe6daca09af61232f5054fcbe49
ju$tin <justin@garofolo.net>
2019-06-03 14:15:32 -0400

    started cdn.upload_movie func

--- uploader/uploader/cdn.py
+++ uploader/uploader/cdn.py
@@ -27,3 +27,83 @@ def reupload_file(url, filename):
         ftp.storbinary("STOR " + filename, file)
     os.remove(filename)
 
+def upload_movie(movie):
+    """
+    Uploads a movie to cdn server, inserts into sql database.
+
+    Parameters:
+        movie (dict): Movie data, retrieved from the flixify.get_movie_data function.
+    """
+
+    # create dir on cdn server & enter it
+    path = movie["url"][1:]
+    ftp.mkd(path)
+    ftp.cwd(path)
+    path_url = cdn_url + path
+
+    # list of values to insert into new row in 'movies' table
+    # title, url, description, duration, thumbnail, preview, qualities, genres, imdb_id, rating, year, subtitles, certification
+    values = list()
+    values.append(movie["title"])
+    values.append(movie["url_short"])
+    values.append(movie["description"])
+    values.append(movie["duration"])
+
+    # upload images
+    def upload_image(name, file):
+        image_url = flixify.ASSETS_URL[:-1] + movie["images"][name]
+        image_file = file + utils.get_extension(image_url)
+        reupload_file(image_url, image_file)
+        values.append(path_url + "/" + image_file)
+
+    upload_image("poster", "thumbnail")         # thumbnail
+    upload_image("preview_large", "preview")    # preview
+
+    # qualities
+    qualities = list()
+    for resolution, url in movie["media"].items():
+
+        # reupload the file to our server
+        file = str(resolution) + ".mp4"
+        reupload_file(url, file)
+
+        # create the quality dict (store resolution & link to file)
+        quality = dict()
+        quality["resolution"] = resolution
+        quality["link"] = path_url + "/" + file
+        qualities.append(quality)
+
+    values.append(json.dumps(qualities))
+
+    # genres, imdb_id, rating, year
+    values.append(json.dumps(movie["genres"]))
+    values.append(movie["imdb_id"])
+    values.append(movie["rating"])
+    values.append(movie["year"])
+
+    # subtitles
+    subtitles = list()
+    for name, sub in  movie["subtitles"].items():
+
+        # note: subtitle name is stored as 3 char (639-2/T), remove last char to convert to 639-1
+        # https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
+        name = name[:-1]
+        sub = sub[0]
+
+        # determine subtitle url on server
+        url = flixify.ASSETS_URL[:-1] + sub["url"]
+        reupload_file(url, name + ".vtt")
+
+        # determine subtitle info to store in database, add to subtitles List
+        subtitle = dict()
+        subtitle["language"] = name
+        subtitle["label"] = utils.get_language_label(name)
+        subtitle["url"] = cdn_url + path + "/{}.vtt".format(name)
+        subtitles.append(subtitle)
+
+    videos.append(json.dumps(subtitles))
+
+    # certification
+    values.append(movie["certification"])
+
+    print(values)
