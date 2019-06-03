8f1d3f1d0dfc0b6f2bdb39d46849d5adc027fb1b
nexzoz <nexzozgaming@gmail.com>
2019-06-02 20:40:23 -0700

    oopsie daisy >_<

--- www.bytesurf.io/scraper/add_movie.php
+++ www.bytesurf.io/scraper/add_movie.php
@@ -1,36 +1,38 @@
-<?php
-
-    require '../inc/server.php';
-    
-    $post_body = file_get_contents('php://input');
-    $data = json_decode($post_body, true);
-
-    $title = $data['title'];
-    $url = $data['url'];
-    $description = $data['description'];
-    $duration = $data['duration'];
-    $thumbnail = $data['thumbnail'];
-    $preview = $data['preview'];
-    $genres = json_encode($data['genres']);
-    $qualities = json_encode($data['qualities']);
-    $imdb_id = $data['imdb_id'];
-    $year = $data['year'];
-    $certification = $data['certification'];
-    $rating = $data['rating'];
-
-    $updateTable = $db->prepare("INSERT INTO movies (title, url, description, duration, thumbnail, preview, qualities, genres, imdb_id, rating, year, certification) VALUES (:title, :url, :description, :duration, :thumbnail, :preview, :qualities, :genres, :imdb_id, :rating, :year, :certification);");
-    $updateTable->bindValue(':title', $title);
-    $updateTable->bindValue(':url', $url);
-    $updateTable->bindValue(':description', $description);
-    $updateTable->bindValue(':duration', $duration);
-    $updateTable->bindValue(':thumbnail', $thumbnail);
-    $updateTable->bindValue(':preview', $preview);
-    $updateTable->bindValue(':qualities', $qualities);
-    $updateTable->bindValue(':genres', $genres);
-    $updateTable->bindValue(':imdb_id', $imdb_id);
-    $updateTable->bindValue(':year', $year);
-    $updateTable->bindValue(':certification', $certification);
-    $updateTable->bindValue(':rating', $rating);
-    $updateTable->execute();
-
+<?php
+
+    require '../inc/server.php';
+    
+    $post_body = file_get_contents('php://input');
+    $data = json_decode($post_body, true);
+
+    $title = $data['title'];
+    $url = $data['url'];
+    $description = $data['description'];
+    $duration = $data['duration'];
+    $thumbnail = $data['thumbnail'];
+    $preview = $data['preview'];
+    $genres = json_encode($data['genres']);
+    $qualities = json_encode($data['qualities']);
+    $subtitles = json_encode($data['subs']);
+    $imdb_id = $data['imdb_id'];
+    $year = $data['year'];
+    $certification = $data['certification'];
+    $rating = $data['rating'];
+
+    $updateTable = $db->prepare("INSERT INTO movies (title, url, description, duration, thumbnail, preview, qualities, genres, imdb_id, rating, year, subtitles, certification) VALUES (:title, :url, :description, :duration, :thumbnail, :preview, :qualities, :genres, :imdb_id, :rating, :year, :subtitles, :certification);");
+    $updateTable->bindValue(':title', $title);
+    $updateTable->bindValue(':url', $url);
+    $updateTable->bindValue(':description', $description);
+    $updateTable->bindValue(':duration', $duration);
+    $updateTable->bindValue(':thumbnail', $thumbnail);
+    $updateTable->bindValue(':preview', $preview);
+    $updateTable->bindValue(':qualities', $qualities);
+    $updateTable->bindValue(':genres', $genres);
+    $updateTable->bindValue(':imdb_id', $imdb_id);
+    $updateTable->bindValue(':year', $year);
+    $updateTable->bindValue(':subtitles', $subtitles);
+    $updateTable->bindValue(':certification', $certification);
+    $updateTable->bindValue(':rating', $rating);
+    $updateTable->execute();
+
 ?>
