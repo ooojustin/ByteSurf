cef35a63c944c122e72bca00b1f6f77bd3f1ca29
Justin <justin@garofolo.net>
2019-07-09 18:34:58 -0400

    docs + started working on upload_movie

--- scraper/scraper/database.py
+++ scraper/scraper/database.py
@@ -2,6 +2,7 @@ import mysql.connector, json, os
 
 # documentation for mysql.connector:
 # https://dev.mysql.com/doc/connector-python/en/connector-python-reference.html
+# https://dev.mysql.com/doc/connector-python/en/connector-python-api-mysqlcursor-execute.html
 
 db = None
 cursor = None
@@ -32,8 +33,35 @@ def connect():
     print("done")
 
 def get_movie(slug):
+    """
+    Gets a movie from our database given the slug.
+
+    Parameters:
+        slug (string): The last part of the URL, used to uniquely identify a movie.
+    Returns:
+        list: The movie row, each columns data is a piece of the list. Returns 'None' if no movie is found.
+    """
     connect()
     query = "SELECT * FROM movies WHERE slug = %s"
     params = tuple([slug])
     cursor.execute(query, params)
     return cursor.fetchone()
+
+def upload_movie(movie):
+    """
+    Uploads a movie to our database.
+
+    Parameters:
+        movie (dict): A dictionary of data returned by flixify.get_movie_data.
+
+    Returns:
+        bool: Indicates whether or not the upload was successful.
+    """
+
+    # colums we need to insert
+    columns = [
+        "slug", "title", "description",
+        "duration", "year", "certification",
+        "genres", "rating", "imdb",
+        "poster", "preview", "media", "updated"
+        ]
